fed58feaa95887cc26ea7be405fc6b5128625449
david scarso <david@remitee.com>
2021-05-14 08:24:01 -0300

    se deja solo un rele

--- XCasaNodos.ino
+++ XCasaNodos.ino
@@ -5,21 +5,18 @@
 // Al reconectar se debe actualizar el estado en la nueva si es que se cambio mientras no habia conexion.
 // ver de colocar un bandera para estado de ultima modificacion, local o web
 
+//new: se cambiar  ala placa sp-01 y este sera el nodo 4
 double pendiente1;
 double pendiente2;
 
 String s_orden01;
-String s_orden02;
 String s_encender01;
-String s_encender02;
 String s_estado01;
-String s_estado02;
 
 String s_clienteNombre;
 
 boolean b_esReset;
 volatile boolean b_estado1ant;
-volatile boolean b_estado2ant;
 
 const char mqtt_wifi_ssid[] = "Fibertel Moco";
 const char mqtt_wifi_pass[] = "00492505506";
@@ -27,28 +24,24 @@ const char mqtt_broker[] = "homeassistant";
 const int mqtt_port = 1883;
 const char mqtt_user[] = "admin";
 const char mqtt_pass[] = "3UreK49";
-const char mqtt_clientid[] = "MQTTC001";
+const char mqtt_clientid[] = "MQTTC004";
 
 WiFiClient mqtt_wifiClient;
 PubSubClient mqtt_client(mqtt_wifiClient);
 
 char mqtt_payload[64];
 
-const int LED_Az = D8;
+const int LED_Az = LED_BUILTIN;// debe se el (1)
 
-const int REL_1 = D7;
-const int REL_2 = D1;
-
-const int Pul1 = D5;
-const int Pul2 = D6;
+const int REL_1 = 0;
+//const int Pul1 = D5;
 
 //para contra efecto rebote o DEBOUNCE
 const int timeThreshold = 200;
 long startTime1 = 0;
-long startTime2 = 0;
 
 void mqtt_setup() {
-  delay(10);;
+  delay(10);
   WiFi.begin(mqtt_wifi_ssid, mqtt_wifi_pass);
   while (WiFi.status() != WL_CONNECTED) delay(500);
   randomSeed(micros());
@@ -59,12 +52,12 @@ void mqtt_setup() {
 
 void mqtt_loop() {
   if (!mqtt_client.connected()) {
-    digitalWrite(LED_Az, LOW);
+    digitalWrite(LED_Az, HIGH);
     mqtt_client.connect(mqtt_clientid, mqtt_user, mqtt_pass);
     mqtt_subscribe();
   }
   if (mqtt_client.connected()) {
-    digitalWrite(LED_Az, HIGH);
+    digitalWrite(LED_Az, LOW);
 
     // ACA COMPARAR ESTADOS SI EL ESTADO DEL LED CAMBIO DURANTE LA DESCONECION, ENVIAR NUEVAMENTE
 
@@ -93,12 +86,10 @@ void mqtt_callback(char* _topic, unsigned char* _payload, unsigned int _payloadl
   double v = mqtt_payload2double(_payload, _payloadlength);
   String vt = mqtt_payload2string(_payload, _payloadlength);
   if (String(_topic) == String(s_encender01))s_orden01 = vt;
-  if (String(_topic) == String(s_encender02))s_orden02 = vt;
 }
 
 void mqtt_subscribe() {
   mqtt_client.subscribe(String(s_encender01).c_str());
-  mqtt_client.subscribe(String(s_encender02).c_str());
 }
 
 void IRAM_ATTR Pulsado1() {
@@ -127,36 +118,9 @@ void IRAM_ATTR Pulsado1() {
   }
 }
 
-void IRAM_ATTR Pulsado2() {
-  if (millis() - startTime2 > timeThreshold)
-  {
-    if (b_estado2ant) {
-      // off en circuito cerrado On es circuito abierto!
-      digitalWrite(REL_2, HIGH);
-
-      b_estado2ant = false;
-      if (mqtt_client.connected()) {
-        mqtt_client.publish(String(s_encender02).c_str(), String(String("OFF")).c_str());
-      }
-
-    } else {
-      digitalWrite(REL_2, LOW);
-
-      b_estado2ant = true;
-      if (mqtt_client.connected()) {
-        mqtt_client.publish(String(s_encender02).c_str(), String(String("ON")).c_str());
-      }
-    }
-    s_orden02 = String("");
-
-    startTime2 = millis();
-  }
-
-}
-
 void Reles() {
   //if (mqtt_client.connected()) {
-  //digitalWrite(LED_Az, HIGH);
+  //digitalWrite(LED_Az, LOW);
   if (String(s_orden01).equals(String("ON"))) {
     digitalWrite(REL_1, LOW);
     b_estado1ant = true;
@@ -168,20 +132,9 @@ void Reles() {
     mqtt_client.publish(String(s_estado01).c_str(), String(String("OFF")).c_str());
   }
   s_orden01 = String("");
-  if (String(s_orden02).equals(String("ON"))) {
-    digitalWrite(REL_2, LOW);
-    b_estado2ant = true;
-    mqtt_client.publish(String(s_estado02).c_str(), String(String("ON")).c_str());
-
-  } else if (String(s_orden02).equals(String("OFF"))) {
-    digitalWrite(REL_2, HIGH);
-    b_estado2ant = false;
-    mqtt_client.publish(String(s_estado02).c_str(), String(String("OFF")).c_str());
-  }
-  s_orden02 = String("");
 
   //} else {
-  // digitalWrite(LED_Az, LOW);
+  // digitalWrite(LED_Az, HIGH);
   //}
 }
 
@@ -189,34 +142,25 @@ void setup()
 {
   pinMode(LED_Az, OUTPUT);
   pinMode(REL_1, OUTPUT);
-  pinMode(REL_2, OUTPUT);
   mqtt_setup();
 
-  pinMode(Pul1, INPUT_PULLUP);
-  pinMode(Pul2, INPUT_PULLUP);
-
-  attachInterrupt(digitalPinToInterrupt(Pul1), Pulsado1, RISING);
-  attachInterrupt(digitalPinToInterrupt(Pul2), Pulsado2, RISING);
+  //pinMode(Pul1, INPUT_PULLUP);
+  //attachInterrupt(digitalPinToInterrupt(Pul1), Pulsado1, RISING);
 
   ESP.wdtDisable();
-  digitalWrite(LED_Az, LOW);
+  digitalWrite(LED_Az, HIGH);
 
   digitalWrite(REL_1, HIGH);
-  digitalWrite(REL_2, HIGH);
 
   // en estado ON es para que si estaba enncendi la luz antes de que se desconectara o se cortara la laimentaicon, se resetea a apagao.
   b_esReset = true;
   b_estado1ant = true;
-  b_estado2ant = true;
-  s_clienteNombre = String("MQTTC001");
+  s_clienteNombre = String("MQTTC004");
 
   s_encender01 = String(s_clienteNombre) + String("/pulsador01");
-  s_encender02 = String(s_clienteNombre) + String("/pulsador02");
   s_estado01 = String(s_clienteNombre) + String("/estado/pulsador01");
-  s_estado02 = String(s_clienteNombre) + String("/estado/pulsador02");
-  s_orden01 = String("");
-  s_orden02 = String("");
 
+  s_orden01 = String("");
 }
 
 
@@ -228,7 +172,6 @@ void loop()
   if (b_esReset) {
     if (mqtt_client.connected()) {
       mqtt_client.publish(String(s_encender01).c_str(), String(String("OFF")).c_str());
-      mqtt_client.publish(String(s_encender02).c_str(), String(String("OFF")).c_str());
     }
     b_esReset = false;
 
